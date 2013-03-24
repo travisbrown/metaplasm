b083779acef15d0ffdbbc31537e6dc54bf6dfd97
Travis Brown <travisrobertbrown@gmail.com>
2013-03-24 13:31:02 -0400

    Some personalization.

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,20 @@ main :: IO ()
 main = hakyll $ do
   let engineConf = defaultEngineConfiguration
 
-  match "images/*" $ do
+  let siteConf = SiteConfiguration
+        { siteRoot = "http://meta.plasm.us/"
+        , siteGaId = "UA-36422511-1"
+        }
+
+  let feedConf = FeedConfiguration
+        { feedTitle = "meta.plasm.us"
+        , feedDescription = "Mostly Scala, some computational humanities"
+        , feedAuthorName = "Travis Brown"
+        , feedAuthorEmail = "travisrobertbrown@gmail.com"
+        , feedRoot = "http://meta.plasm.us"
+        }
+
+  match "images/*.png" $ do
     route idRoute
     compile copyFileCompiler
 
@@ -27,23 +40,25 @@ main = hakyll $ do
   match (fromList ["content/about.rst", "content/contact.markdown"]) $ do
     route $ stripContent `composeRoutes` setExtension "html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= loadAndApplyTemplate "templates/default.html" (siteCtx siteConf)
       >>= relativizeUrls
 
   match "content/posts/*" $ do
+    let ctx = postCtx siteConf
     route $ stripContent `composeRoutes` setExtension "html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html" postCtx
-      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= loadAndApplyTemplate "templates/post.html" ctx
+      >>= saveSnapshot "content"
+      >>= loadAndApplyTemplate "templates/default.html" ctx
       >>= relativizeUrls
 
   create ["archive.html"] $ do
     route idRoute
     compile $ do
       let archiveCtx =
-            field "posts" (\_ -> postList recentFirst) `mappend`
+            field "posts" (\_ -> postList siteConf recentFirst) `mappend`
             constField "title" "Archives" `mappend`
-            defaultContext
+            (siteCtx siteConf)
 
       makeItem ""
         >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
@@ -55,29 +70,42 @@ main = hakyll $ do
     route $ stripContent
     compile $ do
       let indexCtx = field "posts" $ \_ ->
-            postList $ fmap (take 3) . recentFirst
+            postList siteConf $ fmap (take 3) . recentFirst
 
       getResourceBody
         >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" (postCtx siteConf)
         >>= relativizeUrls
+
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = (postCtx siteConf) `mappend` bodyField "description"
+      posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "content/posts/*" "content"
+      renderAtom feedConf feedCtx posts
 
   match "templates/*" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-  dateField "date" "%B %e, %Y" `mappend`
+siteCtx :: SiteConfiguration -> Context String
+siteCtx siteConf =
+  constField "root" (siteRoot siteConf) `mappend`
+  constField "gaId" (siteGaId siteConf) `mappend`
   defaultContext
 
 
 --------------------------------------------------------------------------------
-postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
-postList sortFilter = do
+postCtx :: SiteConfiguration -> Context String
+postCtx siteConf = dateField "date" "%e %B %Y" `mappend` siteCtx siteConf
+
+
+--------------------------------------------------------------------------------
+postList :: SiteConfiguration -> ([Item String] -> Compiler [Item String]) -> Compiler String
+postList siteConf sortFilter = do
   posts <- sortFilter =<< loadAll "content/posts/*"
   itemTpl <- loadBody "templates/post-item.html"
-  list <- applyTemplateList itemTpl postCtx posts
+  list <- applyTemplateList itemTpl (postCtx siteConf) posts
   return list
 
 
